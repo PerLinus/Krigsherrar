0a73522e82750651e540229138fc79ce29c7d632
PerLinus <linus.nilsson83@gmail.com>
2017-01-23 15:45:34 +0100

    Created mainclass

--- core/src/se/snrn/krigsherrar/Krig.java
+++ core/src/se/snrn/krigsherrar/Krig.java
@@ -1,33 +1,34 @@
 package se.snrn.krigsherrar;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Krig extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class Krig extends Game {
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    MainScreen mainScreen;
+
+    SpriteBatch batch;
+
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        mainScreen = new MainScreen(batch);
+        setScreen(mainScreen);
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+
+    }
 }
