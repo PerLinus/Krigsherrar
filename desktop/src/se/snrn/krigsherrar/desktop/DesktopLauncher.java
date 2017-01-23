8d3653f68c0098b8c0fec3e0334e6922f33ae509
PerLinus <linus.nilsson83@gmail.com>
2017-01-23 15:30:30 +0100

    Initial commit

---
+++ desktop/src/se/snrn/krigsherrar/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package se.snrn.krigsherrar.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import se.snrn.krigsherrar.Krig;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Krig(), config);
+	}
+}
